2dc7d565e7c35f5dcee86ab62c0af4546cd2e402
vladvildanov <divinez122@outlook.com>
2024-01-17 14:33:51 +0200

    Initial commit

---
+++ Controller/Adminhtml/Test/Index.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Vlad\Test\Controller\Adminhtml\Test;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
